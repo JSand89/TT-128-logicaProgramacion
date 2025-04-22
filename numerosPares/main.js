2b2688687114f89897cb1a0821a5e793601421db
JSand89 <javier.esand@gmail.com>
2025-04-22 11:31:27 -0500

    dejando solo numeros pares

--- numerosPares/main.js
+++ numerosPares/main.js
@@ -10,15 +10,11 @@ entrada.addEventListener("input",(evento) => {
 // ya tenemos el par, ahora vamos a poner que me diga si es oar y multiplo de 5
 calcular.addEventListener("click",()=>{
     let hijo = document.createElement("li")
-    if(numero%2 == 0 && numero%5 == 0){
-        hijo.innerHTML = numero + " es par y multiplo de 5";
-    }else if(numero%2 == 0){
+    if(numero%2 == 0){
         hijo.innerHTML = numero + " es par";
-    }else if (numero%5 == 0){
-        hijo.innerHTML = numero + " es multiplo de 5";
     }
     else{
-        hijo.innerHTML = numero + " No es par ni multiplo de 5";
+        hijo.innerHTML = numero + " No es par";
     }
     salida.appendChild(hijo);
 });
