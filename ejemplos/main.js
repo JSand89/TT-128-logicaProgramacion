fd4dad6c256d30435a60bb653fe44639898f0a5b
JSand89 <javier.esand@gmail.com>
2025-04-23 11:12:44 -0500

    ejercicio de fibonacci

---
+++ ejemplos/main.js
@@ -0,0 +1,20 @@
+// let factorial = 1
+// for (let i =1; i<=5;i=i+1){
+//     factorial = factorial*i
+//     console.log("iteracion",i)
+// }
+// console.log(factorial)
+
+// let j=0
+// do{
+//     console.log(j);
+//     j++;
+// }while(j<5)
+
+let frutas = ["papaya","banano","pera","piña"];
+frutas[1]= "kiwi";
+
+console.log(frutas.length)
+for (let i=0; i<frutas.length;i++){
+    console.log(frutas[i])
+}
